85e4e2812790686d01a82a5f2882116bde4acbad
JTPeng <404502753@qq.com>
2020-12-18 11:13:22 +0800

    环形链表

---
+++ leetcode/10.环形链表.js
@@ -0,0 +1,34 @@
+/** 环形链表
+ *
+ */
+
+// 污染链表
+var hasCycle = function (head) {
+  while (head) {
+    if (head.target) {
+      return true
+    } else {
+      head.target = true
+    }
+    head = head.next
+  }
+  return false
+}
+
+// 双指针 (快慢指针)
+
+var hasCycle = function (head) {
+  // 环形链表至少有2个值
+  if (!head || !head.next) return false
+  var slow = head
+  var fast = head.next
+  while (slow !== fast) {
+    // 没有环。则快指针会到达终点
+    if (!fast || !fast.next) {
+      return false
+    }
+    slow = slow.next // 走一步
+    fast = fast.next.next // 走两步
+  }
+  return true
+}
